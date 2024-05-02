230bd4f99de3d3ec0ce7da17e0e528d64e72d711
ElieOko <elieoko100@gmail.com>
2024-05-02 07:06:04 +0100

    fix : service axios review code

--- common/service/axios.js
+++ common/service/axios.js
@@ -27,7 +27,7 @@ function useAxiosRequestWithToken(){
   function getApiWithConfigAxios($token = ""){
 
     return  axios.create({
-          baseURL               :/*"https://server.drapeauyamboka.com/api"*/`https://quickbooks.api.intuit.com/v3/company/9341452065431980`,
+          baseURL               :/*"https://server.drapeauyamboka.com/api"*/`https://quickbooks.api.intuit.com/v3/company/9341452194141898`,
           headers   : {
             accept: 'application/json',
             "Content-type"      :   "application/json",
