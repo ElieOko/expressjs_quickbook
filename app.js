d5509a539813fad09602877e86efad54080ad3d4
ElieOko <elieoko100@gmail.com>
2024-06-03 14:17:12 +0100

    fix:cors option 1.1.0

--- app.js
+++ app.js
@@ -36,8 +36,7 @@ var corsOptionsDelegate = function (req, callback) {
 app.use(cors({
   origin: "*",
   methods: ["GET","HEAD","PUT","PATCH","POST","DELETE"],
-  preflightContinue: false,
-  optionsSuccessStatus: 20
+  optionsSuccessStatus: 204
 }));
 // app.options('*', cors())
 app.use(express.json())
