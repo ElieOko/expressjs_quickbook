3e8ea79c271a4467dbd64ba44b331bc5e6fe6184
ElieOko <elieoko100@gmail.com>
2024-06-04 12:06:56 +0100

    test validateur backend for frontend

--- app.js
+++ app.js
@@ -684,6 +684,7 @@ app.post("/api/create/employee",(req,res)=>{
 app.post("/api/create/account",cors(),async(req,resp)=>{
     //  requestAxios.useAxiosRequestWithToken().get("/token/refresh").then(res=>{
         const test  = req.body[0];
+       return resp.status(201).send({message:test})
         qbo.token = req.body[1].token
         qbo.createAccount(test,(err,dataAccount)=>{
           if(err){
