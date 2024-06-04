f639fcad74c7e65a00f1c34fa65c09defff44586
ElieOko <elieoko100@gmail.com>
2024-06-04 13:49:34 +0100

    fix:bug

--- app.js
+++ app.js
@@ -685,19 +685,24 @@ app.post("/api/create/account",cors(),async(req,resp)=>{
     //  requestAxios.useAxiosRequestWithToken().get("/token/refresh").then(res=>{
         const test  = req.body[0];
         qbo.token = req.body[1].token
-        return resp.status(201).send({message:req.body[0].AccountType})
-        qbo.createAccount({
-          "AccountType": `"${req.body[0].AccountType}"`,
-          "Name": `"${req.body[0].Name}"`
-        },(err,dataAccount)=>{
-          if(err){
-            resp.status(201).send({message:err})
-          }
-          else{
-            resp.status(201).send({message:"Enregistrement réussie",data:dataAccount})
-            //dataAccount.Id
-          }
-      })
+        // resp.status(201).send({message:req.body[0].AccountType})
+        requestAxios.getApiWithConfigAxios(qbo.token).post("/account",req.body[0]).then(response=>{
+        resp.status(201).send({message:response.data})
+        }).catch(err=>{
+        resp.send({message:err})
+        })
+      //   qbo.createAccount({
+      //     "AccountType": `"${req.body[0].AccountType}"`,
+      //     "Name": `"${req.body[0].Name}"`
+      //   },(err,dataAccount)=>{
+      //     if(err){
+      //       resp.status(201).send({message:err})
+      //     }
+      //     else{
+      //       resp.status(201).send({message:"Enregistrement réussie",data:dataAccount})
+      //       //dataAccount.Id
+      //     }
+      // })
     // })
     
 })
